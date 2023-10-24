f91b73976d59d6f8bb8def49ef2b1ed7ba57debc
Corneliu Nastas <cnastas@genevamedia.com>
2023-10-24 16:04:20 +0300

    feat: add keycloak

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,41 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthGuard, KeycloakConnectModule, ResourceGuard, RoleGuard } from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://localhost:8080/auth',
+      realm: 'egov',
+      clientId: 'egov-app',
+      secret: '1cWw2WPAlgDvCAaYmaFCEVsyb4fXfeVS',
+    }),
+  ],
+  controllers: [],
+  providers: [
+    // GLOBAL LEVEL RESOURCE GUARD CHECKS FOR JWT TOKEN
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    // This adds a global level resource guard, which is permissive.
+    // Only controllers annotated with @Resource and
+    // methods with @Scopes
+    // are handled by this guard.
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    // This adds a global level role guard, which is permissive.
+    // Used by `@Roles` decorator with the
+    // optional `@AllowAnyRole` decorator for allowing any
+    // specified role passed.
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+
+  ],
 })
 export class AppModule {}
